b936b1595b16968c775a22540095a5acf712d235
PrathmeshModhe9 <201b192@juetguna.in>
2023-12-16 00:17:01 +0530

    fixed some files due to security reasons

---
+++ backend/db/db.js
@@ -0,0 +1,21 @@
+// db.js
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const dotenv = require('dotenv')
+dotenv.config();
+
+const uri = process.env.URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+  ssl: false,
+});
+
+async function connectDatabase() {
+  await client.connect();
+  return client.db('nextspark');
+}
+
+module.exports = { connectDatabase };
