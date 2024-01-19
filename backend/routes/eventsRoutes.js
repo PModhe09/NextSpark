51c7f4b81e43b41dc391d2ff6c028d52e979c3ac
PrathmeshModhe9 <lastry2001@gmail.com>
2024-01-19 16:09:28 +0530

    added routes for update score api

--- backend/routes/eventsRoutes.js
+++ backend/routes/eventsRoutes.js
@@ -4,6 +4,7 @@ const eventControllers = require('../controllers/eventsControllers');
 
 
 router.get('/weekly-leaderboard',eventControllers.weeklyLeaderBoard); // returns weekly leaderboard
+router.post('/update-score',eventControllers.updateScore);
 
 module.exports = router;
 
