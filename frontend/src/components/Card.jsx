7836c50fc67b45d90d80145e938549556f574a09
PrathmeshModhe9 <lastry2001@gmail.com>
2024-02-13 21:00:42 +0530

    updated card ui

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -4,7 +4,7 @@ import { IoBriefcaseOutline } from "react-icons/io5";
 import { GiMoneyStack } from "react-icons/gi";
 
 const Card = ({ data }) => {
-  const { company, logo, role, pay, location, skills, xp, degree, applyLink } = data;
+  const { company, logo, role, pay, location, skills, xp, degree, applyLink, type } = data;
 
   return (
     <section className="text-primary m-5 border-2 border-solid border-gray-300 rounded-2xl
@@ -13,15 +13,16 @@ const Card = ({ data }) => {
         <img src={logo} alt="" className="h-20 w-20 rounded-2xl" />
 
         <div className="">
-          <h4 className="mb-1 font-semibold text-left">{company}</h4>
-          <h3 className="font-semibold mb-2 text-left">{role}</h3>
+          <h4 className=" font-semibold text-left font text-xl">{company}</h4>
+          <h3 className="font-lg text-left">{role}</h3>
+          <h3 className="font-lg text-left">{type}</h3>
           <h2 className=" text-left flex items-center gap-2">
             <GiMoneyStack /> {pay}
+            <div className="flex items-center ml-6"><CiLocationOn /> {location} </div>
           </h2>
           <div className="flex flex-wrap gap-4 mb-2">
-            {xp && <span className="flex items-center gap-2"><IoBriefcaseOutline /> {xp}</span>}
-            <span className="flex items-center gap-2"><IoBriefcaseOutline /> {degree}</span>
-            <span className="flex items-center gap-2"><CiLocationOn /> {location} </span>
+            {xp && <span className="flex items-center gap-2"><IoBriefcaseOutline /> {xp==0?<>Fresher</>:<>{xp}</>}</span>}
+            <div className="flex items-center gap-2"><IoBriefcaseOutline /> {degree}</div>
           </div>
         </div>
       </div>
